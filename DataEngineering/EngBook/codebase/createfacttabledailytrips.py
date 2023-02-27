104b5ca0b762c83f4c1dad568309d23bd5210f00
mensalytics <emailjessmensa@gmail.comm>
2023-02-27 20:15:23 +0000

    another commit

--- DataEngineering/EngBook/codebase/createfacttabledailytrips.py
+++ DataEngineering/EngBook/codebase/createfacttabledailytrips.py
@@ -14,17 +14,30 @@ def create_fact_table(PROJECT_ID, TARGET_TABLE_ID):
         write_disposition = 'WRITE_APPEND'
     )
 
-    sql = """SELECT DATE(start_date) as trip_date,
-          start_station_id,
-          COUNT(trip_id) as total_trips,
-          SUM(duration_sec) as sum_duration_sec,
-          AVG(duration_sec) as avg_duration_sec
-          FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
-          JOIN `{load_date}.raw_bikesharing.stations` stations
-          ON trips.start_station_id = stations.station_id
-          WhERE DATE(start_date) = DATE('{}')
-          GROUP BY trip_date, start_station_id
-          ;""".format(PROJECT_ID=PROJECT_ID, load_date=load_date)
+    # sql = """SELECT DATE(start_date) as trip_date,
+    #       start_station_id,
+    #       COUNT(trip_id) as total_trips,
+    #       SUM(duration_sec) as sum_duration_sec,
+    #       AVG(duration_sec) as avg_duration_sec
+    #       FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
+    #       JOIN `{load_date}.raw_bikesharing.stations` stations
+    #       ON trips.start_station_id = stations.station_id
+    #       WhERE DATE(start_date) = DATE('{}')
+    #       GROUP BY trip_date, start_station_id
+    #       ;""".format(PROJECT_ID=PROJECT_ID, load_date=load_date)
+    
+    sql = """
+        SELECT DATE(start_date) AS trip_date,
+            start_station_id,
+            COUNT(trip_id) AS total_trips,
+            SUM(duration_sec) AS sum_duration_sec,
+            AVG(duration_sec) AS avg_duration_sec
+        FROM `{project_id}.raw_bikesharing.trips` AS trips
+        JOIN `{load_date}.raw_bikesharing.stations` AS stations
+            ON trips.start_station_id = stations.station_id
+        WHERE DATE(start_date) = @load_date
+        GROUP BY trip_date, start_station_id
+    """
     
     query_job = client.query(sql, job_config=job_config) 
 
